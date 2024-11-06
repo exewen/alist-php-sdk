16fa65e22a18c81a89db659f420a1492cb3b82e1
exeweb <exeweb@163.com>
2024-11-06 09:12:39 +0800

    commit_message

--- src/Middleware/AuthMiddleware.php
+++ src/Middleware/AuthMiddleware.php
@@ -10,19 +10,21 @@ use Psr\Http\Message\RequestInterface;
 class AuthMiddleware
 {
 //    private string $config;
+    private $appConfig;
     private $config;
     private $channel;
 
     public function __construct(string $channel, array $config)
     {
-        $appConfig     = Container::getInstance()->get(ConfigInterface::class);
-        $this->channel = $channel;
-        $this->config  = $appConfig->get("http.channels.{$channel}"); //刷新单例到最新配置
+        $this->appConfig = Container::getInstance()->get(ConfigInterface::class);
+        $this->channel   = $channel;
+        $this->config    = $config;
     }
 
     public function __invoke(callable $handler): callable
     {
         return function (RequestInterface $request, array $options) use ($handler) {
+            $this->config    = $this->appConfig->get("http.channels.{$this->channel}"); // 刷新单例到最新配置
             $accessToken     = $this->config['extra']['access_token'] ?? '';
             $modifiedRequest = $request
                 ->withHeader('Authorization', $accessToken);
